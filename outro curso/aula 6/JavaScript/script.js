7b9dacf4e48a20ac4f4233c5f2c6dd968fb630ed
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2022-11-27 14:32:31 -0300

    aula 06 js

---
+++ outro curso/aula 6/JavaScript/script.js
@@ -0,0 +1,9 @@
+var nome = 'Augusto';
+var sobrenome = 'Rodrigues';
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+console.log(nomeCompleto);
+
+document.write("Ele disse 'Olá'");
