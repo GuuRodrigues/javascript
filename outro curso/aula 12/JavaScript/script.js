60ea5389e3da3a98e283572a06c5b73b3cd064a1
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2022-12-11 15:01:09 -0300

    reforçando estudos em JS

---
+++ outro curso/aula 12/JavaScript/script.js
@@ -0,0 +1,15 @@
+var idade = 15;
+
+if (idade>=18) {
+    console.log('O usuário pode fazer a carteira');
+} else {
+    console.log('O usuário não pode tirar carteira');
+}
+
+var nome = 'Augusto';
+
+if (nome== 'Augusto') {
+    console.log(`O seu nome é ${nome}`);
+} else{
+    console.log(`Voce tem outro nome`);
+}
---
+++ outro curso/aula14/JavaScript/script.js
@@ -0,0 +1,11 @@
+var numero = 5;
+
+if(numero === '5') {
+    console.log('O numero é igual a 5');
+} else{
+    console.log('Numero diferente de 5');
+} 
+
+if(numero !== '5'){
+    console.log('O valor 5 é do tipo Number');
+}
