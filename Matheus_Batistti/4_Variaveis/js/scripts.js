26f9bbd826226a025383e01d3492b0906054a187
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-05-02 21:37:00 -0300

    revisando estudos em JavaScript

---
+++ Matheus_Batistti/4_Variaveis/js/scripts.js
@@ -0,0 +1,10 @@
+var teste = 1
+
+nome = 'João'
+
+console.log(teste)
+
+teste = 'Augusto'
+
+console.log(teste)
+console.log(nome)
---
+++ Matheus_Batistti/6_String/js/scripts.js
@@ -0,0 +1,10 @@
+var nome = 'Augusto'
+var sobreNome = 'Rodrigues'
+var nomeCompleto = nome + ' ' + sobreNome
+
+console.log(nome)
+console.log(typeof nome)
+console.log(nomeCompleto)
+
+document.write('Ele disse "Olá!"') // escrevendo no documento
+console.log('Este número: ' + '123')
