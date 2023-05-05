efa2613ff5f1f3126576d329e921a7eab46c5693
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-05-04 22:04:41 -0300

    NULL e Undefined

---
+++ Matheus_Batistti/8_NULL_UNDEFINED/js/scripts.js
@@ -0,0 +1,13 @@
+//hoisting - içamento 
+
+
+var nome = null;
+var sobreNome
+console.log(nome)
+console.log(sobreNome)
+
+
+nome = 'Augusto'
+sobreNome = 'Rodrigues'
+console.log(nome)
+console.log(sobreNome)
