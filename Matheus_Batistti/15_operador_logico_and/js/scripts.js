70049f8670f04397100bd3c9bb3455cea562e4b9
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-05-05 22:35:57 -0300

    operadores lógicos

---
+++ Matheus_Batistti/15_operador_logico_and/js/scripts.js
@@ -0,0 +1,8 @@
+var age = 16
+var name = 'João'
+
+if(name == 'João' && age == 16) {
+    console.log('Jõao pode fazer aula de esgrima')
+} else {
+    console.log('Este não é o joão')
+}
---
+++ Matheus_Batistti/16_operador_logico_or/js/scripts.js
@@ -0,0 +1,12 @@
+var age = 12
+var name = 'João'
+
+if(name == 'João' || age > 14) {
+    console.log('Pode entrar na aula de esgrima')
+} else {
+    console.log('Não pode entrar')
+}
+
+if(name == 'João' && 15 > 20 || 10 == 10) {
+    console.log('Testando')
+}
