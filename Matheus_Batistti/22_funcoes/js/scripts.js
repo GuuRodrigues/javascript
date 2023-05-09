4aa2fd520bd581cdb790e78398333245c1dcd69c
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-05-08 22:05:47 -0300

    funcoes, DOM, tipos de variaveis e metodos numericos

---
+++ Matheus_Batistti/22_funcoes/js/scripts.js
@@ -0,0 +1,24 @@
+function primeiraFuncao() {
+    console.log('Hello world das funções')
+}
+
+primeiraFuncao()
+
+function dizerNome(name) {
+    console.log(`O nome é: ${name}`)
+}
+
+dizerNome('Augusto')
+dizerNome('Djully')
+dizerNome('Belinha')
+
+var nomeDoBancoDeDados = 'Marcone'
+
+dizerNome(nomeDoBancoDeDados)
+
+function soma(a, b) {
+    return a+b
+}
+
+result = soma(1, 8)
+console.log(`O resultado da soma é ${result}`)
---
+++ Matheus_Batistti/23_DOM/js/scripts.js
@@ -0,0 +1,16 @@
+const lista = ['Laranja', 'Maçã', 'Pera', 'Jaca', 'Limão']
+
+const listaUl = document.createElement('ul')
+const body = document.querySelectorAll('body')
+
+body[0].appendChild(listaUl)
+
+const listaNoBody = document.querySelectorAll('ul')
+
+for(var i = 0; i < lista.length; i++) {
+    const liFor = document.createElement('li')
+    const textoLi = document.createTextNode(lista[i])
+
+    liFor.appendChild(textoLi)
+    listaNoBody[0].appendChild(liFor)
+}
