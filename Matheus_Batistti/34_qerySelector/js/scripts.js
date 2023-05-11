2a4d8d70c8e58332f52da61620e39d976fa4aa4d
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-05-11 20:45:27 -0300

    criando e removendo elementos com DOM

---
+++ Matheus_Batistti/34_qerySelector/js/scripts.js
@@ -0,0 +1,12 @@
+//quwrySelectoAll
+
+var itensQuery = document.querySelectorAll('#lista2 li')
+console.log(itensQuery)
+
+//querySelector
+
+var lista = document.querySelector('#lista')
+console.log(lista)
+
+var span = document.querySelector('#paragrafo span')
+console.log(span)
